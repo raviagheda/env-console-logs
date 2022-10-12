ce435fb4877104b6ac7f09e58e3e994160d68d62
Ravi Agheda <rsagheda123@gmail.com>
2022-10-12 11:38:53 +0530

    updated package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env.enable_console_logs);
-
 export const consoleLog = (message?: any, ...optionalParams: any[]) => {
   if (process.env.enable_console_logs === 'true') {
     console.log(message, ...optionalParams);
